0c4cff68fcd75c4a7e22c6df853c57db23e13102
megabanana23 <65601935+megabanana23@users.noreply.github.com>
2021-08-06 12:40:41 -0400

    Add files via upload

---
+++ C81main.js
@@ -0,0 +1,36 @@
+canvas = document.getElementById("myCanvas");
+color = "red";
+
+ctx = canvas.getContex("2d");
+ctx.beginPath();
+ctx.strokeStyle = color;
+ctx.lineWidth = 2;
+ctx.arc(200, 200, 40, 2 * Math.PI);
+ctx.stroke();
+
+canvas.addEventListener("mousedown", my_mousedown);
+
+function my_mousedown(e)
+{
+
+color = document.getElementById("color").Value;
+console.log(color);
+mouse_x = e.clietX - canvas,offsetLeft;
+mouse_y = e.clientY - canvas.offsetTop;
+
+
+console.log("X = " + mouse_x + " ,Y = " + mouse_y);
+circle(mouse_x , mouse_y);
+
+}
+
+function circle(mouse_x , mouse_y)
+{
+
+    ctx.beginPath();
+    ctx.strokeStyle = color;
+    ctx.lineWidth = 2;
+    ctx.arc(mouse_x, mouse_y, 40, 0, 2 * Math.PI)
+    ctx.stroke();
+
+}
